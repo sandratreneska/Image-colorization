43ac9ba6d88763e4fcd1db28adf9db6583b402e2
Sandra Treneska <Sandra@SANDRAT-PC.ryarc.corp>
2021-05-30 17:31:52 +0200

    cGAN

---
+++ cGAN/pretrained_Unet.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+from tensorflow_examples.models.pix2pix import pix2pix
+from config import IMG_WIDTH, IMG_HEIGHT
+
+base_model = tf.keras.applications.MobileNetV2(input_shape=[IMG_WIDTH, IMG_HEIGHT, 3], include_top=False)
+
+# Use the activations of these layers
+layer_names = [
+    'block_1_expand_relu',   # 64x64
+    'block_3_expand_relu',   # 32x32
+    'block_6_expand_relu',   # 16x16
+    'block_13_expand_relu',  # 8x8
+    'block_16_project',      # 4x4
+]
+base_model_outputs = [base_model.get_layer(name).output for name in layer_names]
+
+# Create the feature extraction model
+down_stack = tf.keras.Model(inputs=base_model.input, outputs=base_model_outputs)
+
+#down_stack.trainable = False
+
+up_stack = [
+    pix2pix.upsample(512, 3),  # 4x4 -> 8x8
+    pix2pix.upsample(256, 3),  # 8x8 -> 16x16
+    pix2pix.upsample(128, 3),  # 16x16 -> 32x32
+    pix2pix.upsample(64, 3),   # 32x32 -> 64x64
+]
