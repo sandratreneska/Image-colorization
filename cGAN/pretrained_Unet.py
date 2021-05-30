f12168e976c5ea97f9e57a7d1aa4dc043f1b1be5
Sandra <s_treneska@hotmail.com>
2021-05-30 18:15:50 +0200

    cGAN

--- cGAN/pretrained_Unet.py
+++ cGAN/pretrained_Unet.py
@@ -1,27 +1,114 @@
+# Based on https://www.tensorflow.org/tutorials/images/segmentation
 import tensorflow as tf
-from tensorflow_examples.models.pix2pix import pix2pix
+import numpy as np
 from config import IMG_WIDTH, IMG_HEIGHT
+from keras.optimizers import Adam
+from keras.models import Input
+from keras.models import Model
+from keras.layers import BatchNormalization
+#from PatchGan_Discriminator import define_discriminator
+
+
+def upsample(filters, size, apply_dropout=True):
+    initializer = tf.random_normal_initializer(0., 0.02)
+
+    result = tf.keras.Sequential()
+    result.add(
+    tf.keras.layers.Conv2DTranspose(filters, size, strides=2,
+                                    padding='same',
+                                    kernel_initializer=initializer,
+                                    use_bias=False))
+
+    result.add(tf.keras.layers.BatchNormalization())
+
+    if apply_dropout:
+        result.add(tf.keras.layers.Dropout(0.5))
+
+    result.add(tf.keras.layers.ReLU())
+
+    return result
+
 
 base_model = tf.keras.applications.MobileNetV2(input_shape=[IMG_WIDTH, IMG_HEIGHT, 3], include_top=False)
+print(base_model.summary())
 
 # Use the activations of these layers
 layer_names = [
-    'block_1_expand_relu',   # 64x64
-    'block_3_expand_relu',   # 32x32
-    'block_6_expand_relu',   # 16x16
-    'block_13_expand_relu',  # 8x8
-    'block_16_project',      # 4x4
+    'block_1_expand_relu',   # 128x128
+    'block_3_expand_relu',   # 64x64
+    'block_6_expand_relu',   # 32x32
+    'block_13_expand_relu',  # 16x16
+    'block_16_project',      # 8x8, the bottleneck
 ]
 base_model_outputs = [base_model.get_layer(name).output for name in layer_names]
 
 # Create the feature extraction model
-down_stack = tf.keras.Model(inputs=base_model.input, outputs=base_model_outputs)
+down_stack = tf.keras.Model(inputs=base_model.input, outputs=base_model_outputs, name='mobilenet_down')
 
-#down_stack.trainable = False
+#down_stack.trainable = False  # comment
 
 up_stack = [
-    pix2pix.upsample(512, 3),  # 4x4 -> 8x8
-    pix2pix.upsample(256, 3),  # 8x8 -> 16x16
-    pix2pix.upsample(128, 3),  # 16x16 -> 32x32
-    pix2pix.upsample(64, 3),   # 32x32 -> 64x64
+    upsample(512, 3),                       # 8x8  ->  16x16
+    upsample(256, 3),                       # 16x16 -> 32x32
+    upsample(128, 3, apply_dropout=False),  # 32x32 -> 64x64
+    upsample(64, 3, apply_dropout=False),   # 64x64 -> 128x128
 ]
+
+
+def pretrained_unet_model(output_channels=2):
+    inputs = tf.keras.layers.Input(shape=[IMG_WIDTH, IMG_HEIGHT, 3])
+
+    # Downsampling through the model
+    skips = down_stack(inputs, training=True)  # for dropout
+    x = skips[-1]
+    skips = reversed(skips[:-1])  # outputs to be concatenated
+
+    # Upsampling and establishing the skip connections
+    for up, skip in zip(up_stack, skips):
+        x = up(x, training=True)  # for batchnorm and dropout
+        concat = tf.keras.layers.Concatenate()
+        x = concat([x, skip])
+
+    # This is the last layer of the model
+    last = tf.keras.layers.Conv2DTranspose(
+        output_channels, 3, strides=2,
+        padding='same')   # 128x128 -> 256x256
+
+    x = last(x)
+
+    return tf.keras.Model(inputs=inputs, outputs=x, name='pretrained_unet')
+
+
+# define the combined generator and discriminator model, for updating the generator
+def define_gan_pretrained(g_model, d_model, image_shape=(IMG_WIDTH, IMG_HEIGHT, 1)):
+    # make weights in the discriminator not trainable
+    for layer in d_model.layers:
+        if not isinstance(layer, BatchNormalization):
+            layer.trainable = False
+    # define the source image
+    in_src = Input(shape=image_shape)
+    #print(in_src.shape)
+
+    # stack 3 grayscale images for generator input
+    g_input = tf.stack((tf.reshape(in_src, (-1, 256, 256)), tf.reshape(in_src, (-1, 256, 256)), tf.reshape(in_src, (-1, 256, 256))), axis=3)
+    #print(g_input.shape)
+    # connect the source image to the generator input
+    gen_out = g_model(g_input)
+
+    # connect the source input and generator output to the discriminator input
+    dis_out = d_model([in_src, gen_out])
+    # src image as input, generated image and classification output
+    model = Model(in_src, [dis_out, gen_out], name='full_gan_pretrained')
+    # compile model
+    opt = Adam(lr=0.0002, beta_1=0.5)
+    model.compile(loss=['binary_crossentropy', 'mae'], optimizer=opt, loss_weights=[1, 100])
+    return model
+
+'''
+d_model = define_discriminator()
+g_model = pretrained_unet_model()
+print(g_model.summary())
+gan_model_pretrained = define_gan_pretrained(g_model, d_model)
+print(gan_model_pretrained.summary())
+'''
+
---
+++ train_pretrained_Gan.py
@@ -0,0 +1,101 @@
+import os
+import glob
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from config import BATCH_SIZE, EPOCHS, PATCH_SHAPE, GENERATOR_PRE_DIR, DISCRIMINATOR_PRE_DIR
+from data_generators import create_train_generator, create_val_generator
+from cGAN.PatchGan_Discriminator import define_discriminator
+from cGAN.pretrained_Unet import pretrained_unet_model
+from cGAN.pretrained_Unet import define_gan_pretrained
+from utils import summarize_performance
+os.environ['TF_MIN_LOG_LEVEL'] = '2'  # minimize some log messages
+
+if __name__ == '__main__':
+
+    print("Tensorflow version:", tf.__version__)
+
+    # Check if GPU available
+    if tf.test.gpu_device_name():
+        print('Default GPU Device:{}'.format(tf.test.gpu_device_name()))
+    else:
+        print("Please install GPU version of TF")
+
+    # Create train and validation data generators
+    train_data_gen, num_train_samples = create_train_generator()
+    val_data_gen, num_val_samples = create_val_generator()
+
+    # Define, compile and fit model
+
+    # if saved, load latest generator
+    list_of_gens = glob.glob(GENERATOR_PRE_DIR + '*')
+    if len(list_of_gens) > 0:
+        latest_gen = max(list_of_gens, key=os.path.getctime)
+        print(latest_gen)
+        g_model = keras.models.load_model(latest_gen)
+        g_model.trainable = True # make layers of mobilenet trainable
+    else:
+        g_model = pretrained_unet_model()
+
+    # if saved, load latest discriminator
+    list_of_disc = glob.glob(DISCRIMINATOR_PRE_DIR + '*')
+    if len(list_of_disc) > 0:
+        latest_disc = max(list_of_disc, key=os.path.getctime)
+        print(latest_disc)
+        d_model = keras.models.load_model(latest_disc)
+    else:
+        d_model = define_discriminator()
+
+    # define the composite model
+    gan_model = define_gan_pretrained(g_model, d_model)
+    # summarize the model
+    #gan_model.summary()
+
+    # calculate the number of steps per training epoch
+    steps_per_epo = int(num_train_samples / BATCH_SIZE)
+    print('Steps per epoch: ', steps_per_epo)
+    # calculate the number of training iterations, total steps
+    n_steps = steps_per_epo * EPOCHS
+    print('Total number of steps: ', n_steps)
+
+    # manually enumerate epochs
+    for i in range(n_steps):
+
+        # select a batch of real samples
+        X_real, Y_real = next(train_data_gen)
+        y_real_ones = np.ones((BATCH_SIZE, PATCH_SHAPE, PATCH_SHAPE, 1))
+
+        # Stack three grayscale images for generator input
+        G_X_real = tf.stack((tf.reshape(X_real, (-1, 256, 256)), tf.reshape(X_real, (-1, 256, 256)), tf.reshape(X_real, (-1, 256, 256))), axis=3)
+
+        # generate a batch of fake samples
+        Y_fake = g_model.predict(G_X_real)
+        y_fake_zeros = np.zeros((BATCH_SIZE, PATCH_SHAPE, PATCH_SHAPE, 1))
+
+        # update discriminator for real samples
+        d_loss1 = d_model.train_on_batch([X_real, Y_real], y_real_ones)
+        # update discriminator for generated samples
+        d_loss2 = d_model.train_on_batch([X_real, Y_fake], y_fake_zeros)
+        # update the generator
+        # for output we want all ones from the disc. and the real color img (X, Y)
+        g_loss, _, _ = gan_model.train_on_batch(X_real, [y_real_ones, Y_real])
+        # summarize performance
+        print('Training step %d, d1[%.3f] d2[%.3f] g[%.3f]' % (i + 1, d_loss1, d_loss2, g_loss))
+
+        # New epoch starting
+        if (i+1) % steps_per_epo == 0:
+            # Start generator again for new epoch
+            print('Init generator again...')
+            train_data_gen, _ = create_train_generator()
+
+            # Plots, save model, validation
+            summarize_performance(i, g_model, d_model, train=True, pretrained=True)
+            summarize_performance(i, g_model, d_model, train=False, pretrained=True)
+
+        '''
+        # Every 3000 steps or last step
+        if (i + 1) % 3000 == 0 or (i + 1) / EPOCHS == steps_per_epo: # change
+            # Plots, save model, validation
+            summarize_performance(i, g_model, d_model, train=True, pretrained=True)
+            summarize_performance(i, g_model, d_model, train=False, pretrained=True)
+        '''
